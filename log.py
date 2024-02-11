6d40b08d09da83c2df208f428ff727fde72e14ec
haribote <116637919+h4ribote@users.noreply.github.com>
2024-02-11 19:06:33 +0900

    Add files via upload

---
+++ log.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+api_token = input("api_token: ")
+
+response = requests.post('http://tak-api.h4ribote.net/log.php', data={'api_token': api_token})
+
+data = (response.text)
+
+data = json.loads(data)
+
+print(data)
+
+"""
+if ('error' in data):
+    print(data['error'])
+else:
+    for data2 in data:
+        print(data2['amount']+data2['unit']+"\n")
+"""
+input("press enter to exit")
